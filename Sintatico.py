fb4377811af59b5314a3d8b6bc6152049b43bcc9
gabitv100 <gabitv100@hotmail.com>
2021-10-04 23:31:37 -0400

    Update Sintatico.py

--- Sintatico.py
+++ Sintatico.py
@@ -2,78 +2,124 @@ import Lexico
 
 
 
-class Sintatico:
-
-    def __init__(self):
-        self.termo = ''
 
 
-    #FUNCOES <------------------------------------------------ <-
-    def P():
+lex = Lexico.Lex()
+pos = 0
+simbolo=''
 
-    def C():
+arquivo = open('teste.txt', 'r')
+unica_string = arquivo.read()
+arquivo.close()
+# print(unica_string)
+posicao = 0
+estado = 0
 
-    def DC():
+for i in unica_string:
+    estado = lex.nextToken(estado=estado, c=i, self='')
+    print('Estado atual -> ' + str(estado))
+print(lex.Tokens)
+listaTokens = (lex.Tokens)
 
-    def DCV():
-
-    def TPV():
-
-    def VAR():
-
-    def MVAR():
-
-    def CMS():
-
-    def MCM():
-
-    def CM():
-
-    def COND():
-
-    def REL():
-
-    def EXP():
-
-    def TERM():
-
-    def OPU():
-
-    def FATOR():
-
-    def OTERM():
-
-    def OPAD():
-
-    def MFATOR():
-
-    def OPMUL():
-
-    def PFALSA():
+def disparaErro(esperado):
+    print('Erro sintatico, era esperado: ' + esperado)
+    return 0
 
 
+# FUNCOES <------------------------------------------------ <-
+def DC(lex):
+    print(lex.simbolo)
+
+def CMS(lex):
+    print(lex.simbolo)
+
+
+def C(lex):
+    if lex.tipo == 'RESERVADA':
+        DC(lex)
+        getSimbolo(lex)
+        if lex.simbolo == 'begin':
+            getSimbolo(lex)
+            CMS(lex)
+            getSimbolo(lex)
+            if lex.simbolo == 'end':
+                print('>>>>>> Sucesso de compilação! <<<<<<<<')
+            else:
+                disparaErro('end')
+        disparaErro('begin')
+    elif lex.simbolo == 'begin': #NAO HOUVE VARIAVEIS
+        getSimbolo(lex)
+        CMS(lex)
+        getSimbolo(lex)
+        if lex.simbolo == 'end':
+            print('>>>>>> Sucesso de compilação! <<<<<<<<')
+        else:
+            disparaErro('end')
+    else:
+        disparaErro('declaração de variavel ou começo de programa')
 
 
 
-    def simbolo(lex):
-        for i in lex.Tokens:
-            simbolo = i
-            print(simbolo)
-            # print("SINTATICO ::: " + str(simbolo))
+def sP(lex):
+    getSimbolo(lex)
+    print('SIMBOLO : ' + lex.simbolo)
+    if(lex.simbolo == 'program'):
+        getSimbolo(lex)
+        if lex.tipo == 'VARIAVEL':
+            getSimbolo(lex)
+            C(lex)
+        else:
+            disparaErro('Nome de programa')
+    else:
+        disparaErro('program')
 
-    arquivo = open('teste.txt', 'r')
-    unica_string = arquivo.read()
-    arquivo.close()
-    # print(unica_string)
-    posicao = 0
-    estado = 0
+def getSimbolo(lex):
+    simbolo = lex.Tokens[lex.pos]['termo']
+    tipo = lex.Tokens[lex.pos]['tipo']
+    lex.simbolo=simbolo
+    lex.tipo=tipo
+    lex.pos = lex.pos + 1
 
-    lex = Lexico.Lex()
 
-    for i in unica_string:
-        estado = lex.nextToken(estado=estado, c=i,self='')
-        print('Estado atual -> ' + str(estado))
-    print(lex.Tokens)
 
-    simbolo(lex)
-
+sP(lex)
+#
+# def DC():
+#
+# def DCV():
+#
+# def TPV():
+#
+# def VAR():
+#
+# def MVAR():
+#
+# def CMS():
+#
+# def MCM():
+#
+# def CM():
+#
+# def COND():
+#
+# def REL():
+#
+# def EXP():
+#
+# def TERM():
+#
+# def OPU():
+#
+# def FATOR():
+#
+# def OTERM():
+#
+# def OPAD():
+#
+# def MFATOR():
+#
+# def OPMUL():
+#
+# def PFALSA():
+#
+#
